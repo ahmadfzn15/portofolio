88191e8585a6ddabf1a48d4dffae52ce304acc70
Ahmad Fauzan <ahmadfzn2005@gmail.com>
2024-09-29 21:19:20 +0700

    new commit

--- app/project/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Page() {
-  return (
-    <div className="w-full flex flex-col p-10 gap-2">
-      <div className="">
-        <img src="/img/kasir.png" className="h-[80vh]" alt="" />
-      </div>
-    </div>
-  );
-}
--- components/footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="relative bottom-0 w-full p-5 mt-10 bg-black text-white">
-      <h1 className="text-center text-sm">Copyright 2024 ahmad fauzan</h1>
-    </footer>
-  );
-}
